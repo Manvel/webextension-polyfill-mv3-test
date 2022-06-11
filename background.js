865a088cbbcf1fbdcd8fefe40a44b3e74d5648af
Manvel Saroyan <saroyanm@gmail.com>
2022-06-11 17:09:12 +0200

    added polyfill as a dependency and script execution

---
+++ background.js
@@ -0,0 +1,16 @@
+const browser = require("webextension-polyfill");
+
+browser.action.onClicked.addListener(async tab => {
+  try {
+    await browser.scripting.executeScript({
+      target: {
+        tabId: tab.id,
+      },
+      func: () => {
+        alert("browser.scripting.executeScript works");
+      },
+    });
+  } catch (err) {
+    console.error(`failed to execute script: ${err}`);
+  }
+});
